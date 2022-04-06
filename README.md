a7e9d7c933a788bfe71abbf3412e594e6879cf47
1209574292@qq.com <1209574292@qq.com>
2022-04-06 10:39:27 +0800

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# README.md 
+# 这是一个基于django的博客系统
---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,10 @@
+from pickle import TRUE
+from .base import *
+import os
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3')
+    }
+}
